3237d9c9d0968882bc447912308915e1f4c5303b
Melanie Schicker <melanie.schicker@gmail.com>
2019-05-19 21:52:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,25 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["fire", "water", "earth", "air"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array << "ice"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift("lightning")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
