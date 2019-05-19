95f1fb5ac440940c48b09592413bd34d70dfaf37
Melanie Schicker <melanie.schicker@gmail.com>
2019-05-19 22:08:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   four_elements = ["fire", "water", "earth", "air"]
-  four_elements << "ice"
+  four_elements << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
