5c4becffd66f1ea070ff3d1a0e00e2c7b338ea3a
Melanie Schicker <melanie.schicker@gmail.com>
2019-05-19 22:15:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["fire", "water", "earth", "air"]
+  ["wow", "I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
