c471d390a57da10b13b8df1dc8aecae0e7a7f28e
Melanie Schicker <melanie.schicker@gmail.com>
2019-05-19 22:45:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  create_an_array
+  array = ["wow", "I", "am", "really", "learning"]
 end
 
 def retrieve_last_element_from_array(array)
