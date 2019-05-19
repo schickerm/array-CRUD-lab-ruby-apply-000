399ed01d17e57c8c9485c16cffae4713095911fc
Melanie Schicker <melanie.schicker@gmail.com>
2019-05-19 21:58:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array.push("ice")
-end
+  push("ice")
+endcreate_an_array.
 
 def add_element_to_start_of_array(array, element)
   create_an_array.unshift("ice")
