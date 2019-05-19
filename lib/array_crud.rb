6fce2d4e3aab6f261eb612aa6ab8fe12f95b751d
Melanie Schicker <melanie.schicker@gmail.com>
2019-05-19 22:11:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
   element = "arrays!"
-  
+  expect(add_element_to_end_of_array(array, element).last).to eq("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
