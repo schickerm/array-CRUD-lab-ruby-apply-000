8f5557ef09fc56fb365d27097cf793e951e606ab
Melanie Schicker <melanie.schicker@gmail.com>
2019-05-19 22:22:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,8 +16,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  remove_element_from_end_of_array.pop
+    remove_element_from_end_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
